f5e381514b2139c0513f9a99cd4936262312e00d
Chris Umbel <chris@chrisumbel.com>
2020-09-02 12:11:20 -0400

    browser launch is from main setup

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,6 @@
 Start-Process powershell -ArgumentList "-NoExit -Command ""cd c:\users\WorkshopStudent\src\Workshop"""
 
+Start-Process chrome -ArgumentList "https://docs-dev.steeltoe.io/session.html?redirect=/labs/modernize-dotnet"
+
 devenv c:\users\WorkshopStudent\src\Workshop\Workshop.sln
 code c:\users\WorkshopStudent\src\Workshop\
