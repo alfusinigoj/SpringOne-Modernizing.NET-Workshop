96ce466a1dea00af5c8ce18de77f29d5ea49e20b
Chris Umbel <chris@chrisumbel.com>
2020-09-02 11:52:52 -0400

    starts vs code and studio

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,4 @@
 Start-Process powershell -ArgumentList "-NoExit -Command ""cd c:\users\WorkshopStudent\src\Workshop"""
+
+devenv c:\users\WorkshopStudent\src\Workshop\Workshop.sln
+code c:\users\WorkshopStudent\src\Workshop\
