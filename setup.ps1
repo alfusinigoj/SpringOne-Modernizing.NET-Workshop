40978221f34d30ff6ae18fdf1b1267e2326c5787
Chris Umbel <chris@chrisumbel.com>
2020-09-03 11:03:41 -0400

    starts at first exercise

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 Start-Process powershell -ArgumentList "-NoExit -Command ""cd c:\users\WorkshopStudent\src\Workshop"""
 
-Start-Process chrome -ArgumentList "https://docs-dev.steeltoe.io/session.html?redirect=/labs/modernize-dotnet"
+Start-Process chrome -ArgumentList "https://docs-dev.steeltoe.io/session.html?redirect=/labs/modernize-dotnet/exercise1.html"
 
 devenv c:\users\WorkshopStudent\src\Workshop\Workshop.sln
 code c:\users\WorkshopStudent\src\Workshop\
